71034edaf3fa7e52fad3e5fff2bdecf838d1a95c
Leanna Szypowski <leannaszypowski@gmail.com>
2023-02-27 13:13:17 -0600

    Fix scroll animation bugs

--- scripts/scrollObserver.js
+++ scripts/scrollObserver.js
@@ -1,14 +1,20 @@
-const sections = [...document.querySelectorAll("section")];
+const floatIn = [...document.querySelectorAll(".inactive")];
+const options = {
+  threshold: [0.25, 0.5, 0.75, 1],
+};
 
 const io = new IntersectionObserver((entries) => {
+  console.log(entries);
   entries.forEach((entry) => {
-    if (entry.intersectionRatio > 0) {
+    console.log(entry.intersectionRatio);
+    if (entry.intersectionRatio >= 0.5) {
       // Add 'active' class if observation target is inside viewport
+      entry.target.classList.remove("inactive");
       entry.target.classList.add("active");
     }
   });
-});
+}, options);
 
-sections.forEach((section) => {
+floatIn.forEach((section) => {
   io.observe(section);
 });
