ecd473a37a584d71c9c1261c8a00729965dbbc4b
Leanna Szypowski <leannaszypowski@gmail.com>
2023-03-08 12:51:12 -0600

    Rework file layout

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,13 @@ const { defineConfig } = require("vite");
 import { resolve } from "path";
 
 module.exports = defineConfig({
-  root: "src",
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/index.html"),
-        about: resolve(__dirname, "src/about.html"),
-        about: resolve(__dirname, "src/projects.html"),
-        about: resolve(__dirname, "src/skills.html"),
+        main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about.html"),
+        projects: resolve(__dirname, "projects.html"),
+        skills: resolve(__dirname, "skills.html"),
       },
     },
   },
