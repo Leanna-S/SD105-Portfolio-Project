f57c2fe73a7a23bdab5ae7101fddc1ac430b6bfe
Leanna Szypowski <leannaszypowski@gmail.com>
2023-05-26 12:41:01 -0500

    Update art images

--- scripts/cardEffect.js
+++ scripts/cardEffect.js
@@ -3,30 +3,40 @@ const previousCardButton = document.getElementById("get-previous-card");
 const cardContainer = document.querySelector(".card-container");
 const cards = document.querySelectorAll(".card img");
 const art = [
-  "IMG_1111",
-  "IMG_1112",
-  "IMG_1113",
-  "IMG_1114",
-  "IMG_0775",
-  "IMG_0854",
-  "IMG_0880",
-  "IMG_0881",
-  "IMG_0890",
-  "IMG_0891",
-  "IMG_0923",
-  "IMG_0924",
-  "IMG_0936",
-  "IMG_0972",
-  "IMG_0977",
-  "IMG_0987",
-  "IMG_0993",
-  "IMG_1032",
-  "IMG_1033",
-  "IMG_1034",
-  "IMG_1035",
-  "IMG_1036",
-  "IMG_1037",
-  "IMG_1039",
+  "/art/IMG_1111.jpg",
+  "/art/IMG_1113.jpg",
+  "/art/IMG_0775.jpg",
+  "/art/IMG_0987.jpg",
+  "/art/IMG_0923.jpg",
+  "/art/IMG_1114.jpg",
+  "/art/IMG_1315.jpg",
+  "/art/IMG_0854.jpg",
+  "/art/IMG_1037.jpg",
+  "/art/IMG_0972.jpg",
+  "/art/IMG_1033.jpg",
+  "/art/IMG_1314.jpg",
+  "/art/IMG_1312.jpg",
+  "/art/IMG_1034.jpg",
+  "/art/IMG_1035.jpg",
+  "/art/IMG_1112.jpg",
+  "/art/IMG_0890.jpg",
+  "/art/IMG_1310.jpg",
+  "/art/IMG_1313.jpg",
+  "/art/IMG_0798.jpg",
+  "/art/IMG_0880.jpg",
+  "/art/IMG_1209.jpg",
+  "/art/IMG_0924.jpg",
+  "/art/IMG_1032.jpg",
+  "/art/IMG_1318.jpg",
+  "/art/IMG_1400.jpg",
+  "/art/IMG_0891.jpg",
+  "/art/IMG_1039.jpg",
+  "/art/IMG_0993.jpg",
+  "/art/IMG_0881.jpg",
+  "/art/IMG_1316.jpg",
+  "/art/IMG_0936.jpg",
+  "/art/IMG_0977.jpg",
+  "/art/IMG_1036.jpg",
 ];
 
 function shuffle(array) {
@@ -64,7 +74,7 @@ function nextCard() {
     currentPhoto = currentPhoto + 4;
   }
   cards.forEach((card, index) => {
-    card.src = `./images/${art[currentPhoto + index]}.jpg`;
+    card.src = `${art[currentPhoto + index]}`;
     changeCard(card);
   });
 }
@@ -77,7 +87,7 @@ function previousCard() {
   }
 
   cards.forEach((card, index) => {
-    card.src = `./images/${art[currentPhoto - index - 1]}.jpg`;
+    card.src = `${art[currentPhoto - index - 1]}`;
     changeCard(card);
   });
 }
